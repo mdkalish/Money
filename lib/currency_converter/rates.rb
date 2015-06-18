d4224d93ca9ef99906814532cefa91d2e453787a
mdkalish <mdkalish4git@gmail.com>
2015-06-18 15:48:40 +0200

    Account for currencylayer free plan limitations: Add recurrent conversion and reciprocal rate

--- lib/currency_converter/rates.rb
+++ lib/currency_converter/rates.rb
@@ -1,54 +1,25 @@
 module CurrencyConverter
   class Rates
-    KNOWN_CURRENCIES = ["usd", "eur", "gbp", "chf", "jpy", "pln"]
+    def self.index(base_currency, target_currency)
+      base_currency.upcase!
+      target_currency.upcase!
+      # free plan on currencylayer allows only USD as the base currency
+      if target_currency == 'USD'
+        rate = BigDecimal(1) / self.index(target_currency, base_currency)
+      else
+        rate = get_rate(base_currency, target_currency)
+      end
+      rate.round(6)
+    end
 
-    def self.index(c1, c2)
-      c1.upcase!; c2.upcase!
-      rate = {
-        "USD_USD"=>1,
-        "USD_EUR"=>0.8896,
-        "USD_GBP"=>0.6506,
-        "USD_CHF"=>0.9337,
-        "USD_JPY"=>124.3545,
-        "USD_PLN"=>3.709,
-        "EUR_USD"=>1.1239,
-        "EUR_EUR"=>1,
-        "EUR_GBP"=>0.7313,
-        "EUR_CHF"=>1.0495,
-        "EUR_JPY"=>139.7545,
-        "EUR_PLN"=>4.1683,
-        "GBP_USD"=>1.5367,
-        "GBP_EUR"=>1.3674,
-        "GBP_GBP"=>1,
-        "GBP_CHF"=>1.4343,
-        "GBP_JPY"=>191.355,
-        "GBP_PLN"=>5.6999,
-        "CHF_USD"=>1.0716,
-        "CHF_EUR"=>0.9529,
-        "CHF_GBP"=>0.6975,
-        "CHF_CHF"=>1,
-        "CHF_JPY"=>133.3307,
-        "CHF_PLN"=>3.9761,
-        "JPY_USD"=>0.008,
-        "JPY_EUR"=>0.0072,
-        "JPY_GBP"=>0.0052,
-        "JPY_CHF"=>0.0075,
-        "JPY_JPY"=>1,
-        "JPY_PLN"=>0.0298,
-        "PLN_USD"=>0.2699,
-        "PLN_EUR"=>0.2397,
-        "PLN_GBP"=>0.1754,
-        "PLN_CHF"=>0.2521,
-        "PLN_JPY"=>33.5366,
-        "PLN_PLN"=>1
-      }["#{c1}_#{c2}"]
-      BigDecimal(rate.to_s) unless rate.nil?
+    def self.get_rate(base_currency, target_currency)
+      BigDecimal(RatesFetcher.response_json['quotes']["#{base_currency}#{target_currency}"].to_s)
     end
   end
 end
 
 class Object
-  def Rates(c1, c2)
-    CurrencyConverter::Rates.index(c1, c2)
+  def Rates(base_currency, target_currency)
+    CurrencyConverter::Rates.index(base_currency, target_currency)
   end
 end
--- spec/rates_spec.rb
+++ spec/rates_spec.rb
@@ -1,17 +1,14 @@
 describe Rates do
+  let(:path) { File.expand_path('../fixtures/response', __FILE__) }
+  let(:response) { StringIO.new(File.read(path))}
+
   describe '#index' do
-    it 'returns correct ratio' do
-      expect(Rates.index('pln', 'eur')).to be_instance_of(BigDecimal)
-      expect(Rates.index('pln', 'eur').to_s).to match(/\d+\.\d+/)
-    end
+    before { allow(RatesFetcher).to receive(:response).and_return(response) }
+    it { expect(Rates.index('pln', 'eur')).to be_instance_of(BigDecimal) }
+    it { expect(Rates.index('pln', 'eur').to_s).to match(/\d+\.\d+/) }
   end
 
-  describe 'Rates(c1, c2)' do
-    let(:rate) { BigDecimal.new('0.3') }
-    before { allow(Rates).to receive(:index).with('pln', 'usd').and_return(rate) }
-
-    it 'returns correct ratio' do
-      expect(Rates('pln', 'usd')).to eq(rate)
-    end
+  describe 'Rates(base_currency, target_currency)' do
+    it { expect(Rates('pln', 'usd')).to eq( Rates.index('pln', 'usd') ) }
   end
 end
