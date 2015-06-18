db6538f16301ad382101f057d42315a350f843a1
mdkalish <mdkalish4git@gmail.com>
2015-06-18 15:52:40 +0200

    Add dotenv, update gemspec, require new files

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -1,6 +1,11 @@
+require 'dotenv'
+Dotenv.load
 require 'currency_converter/exceptions/invalid_currency'
+require 'currency_converter/rates_fetcher'
 require 'currency_converter/exchange'
 require 'currency_converter/rates'
 require 'currency_converter/money'
 require 'bigdecimal'
 require 'bigdecimal/util'
+require 'open-uri'
+require 'json'
